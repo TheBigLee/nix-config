d688428b76fff50d8c245237153b7cdcbbf8960d
Nicolas Bigler <nicolas.bigler@vshn.ch>
2026-02-25 11:49:54 +0100

    Add sync folder
    Signed-off-by: Nicolas Bigler <nicolas.bigler@vshn.ch>

--- modules/home-manager/tools/syncthing.nix
+++ modules/home-manager/tools/syncthing.nix
@@ -7,7 +7,6 @@
 
     # Preserve devices/folders added or modified via the web UI
     overrideDevices = false;
-    overrideFolders = false;
 
     guiAddress = "0.0.0:8384";
 
@@ -16,6 +15,14 @@
     settings = {
       gui.user = config.hostSpec.username;
       options.urAccepted = -1; # Opt out of anonymous usage reporting
+
+      folders = {
+        "obsidian" = {
+          path = "/home/${config.hostSpec.username}/obsidian";
+          label = "Obsidian";
+        };
+      };
     };
+
   };
 }
