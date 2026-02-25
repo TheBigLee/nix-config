74cb9b06a2bd001ad14c4f7aa9315558edf8d3dc
Nicolas Bigler <nicolas.bigler@vshn.ch>
2026-02-25 11:33:40 +0100

    Add syncthing
    Signed-off-by: Nicolas Bigler <nicolas.bigler@vshn.ch>

---
+++ modules/home-manager/tools/syncthing.nix
@@ -0,0 +1,21 @@
+{ config, ... }:
+{
+  sops.secrets."syncthing/gui-password" = {};
+
+  services.syncthing = {
+    enable = true;
+
+    # Preserve devices/folders added or modified via the web UI
+    overrideDevices = false;
+    overrideFolders = false;
+
+    guiAddress = "0.0.0:8384";
+
+    passwordFile = config.sops.secrets."syncthing/gui-password".path;
+
+    settings = {
+      gui.user = config.hostSpec.username;
+      options.urAccepted = -1; # Opt out of anonymous usage reporting
+    };
+  };
+}
